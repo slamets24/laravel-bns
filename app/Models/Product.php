723a21c36f685e2b0aa09c77350e6e11d7607be7
Slamet Septiawan <slametseptiawan9d@gmail.com>
2024-10-17 12:14:59 +0700

    fix: ketinggalan ngisinya

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'category',
+        'url_shopee',
+        'url_tokped',
+        'url_wa',
+        'id_url_image',
+        'id_color',
+        'id_user'
+    ];
 }
